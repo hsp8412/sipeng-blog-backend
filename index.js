2299a70c560cb29f374d24fcc2ad0822be839bdd
Sipeng He <hsp8412@hotmail.com>
2022-06-03 17:49:25 -0600

    modify index.js

--- index.js
+++ index.js
@@ -2,9 +2,14 @@ const express = require("express");
 const app = express();
 
 require("./startup/routes")(app);
-require("./startup/db")();
+require("./startup/db")()
 
 const port = process.env.PORT || 4000;
+
+app.get("/",function (res,req){
+  res.send("This is the Node.js backend for Sipeng He's personal blog app.")
+})
+
 app.listen(port, () => {
   console.log("Listening at " + port);
 });
