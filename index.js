a3f5fed05814779a732b175ec79ed7ed91c620ba
Sipeng He <hsp8412@hotmail.com>
2022-06-03 17:55:01 -0600

    modify routes

--- index.js
+++ index.js
@@ -6,10 +6,6 @@ require("./startup/db")()
 
 const port = process.env.PORT || 4000;
 
-app.get("/",function (res,req){
-  res.send("This is the Node.js backend for Sipeng He's personal blog app.")
-})
-
 app.listen(port, () => {
   console.log("Listening at " + port);
 });
--- startup/routes.js
+++ startup/routes.js
@@ -5,6 +5,9 @@ const post = require("../routes/post");
 const auth = require("../routes/auth");
 
 module.exports = function (app) {
+  app.get('/', function(req, res, next) {
+    res.status(200).send("Hi, It works!")
+  });
   app.use(cors());
   app.use(express.json());
   app.use(express.urlencoded({ extended: false }));
